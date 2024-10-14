92eca75698cfe49be5724880efc32653dbbb26ce
Rale13 <rastko.kostic@gmail.com>
2024-10-14 21:41:45 +0200

    billing info basic finished

--- tests/billing-info.spec.js
+++ tests/billing-info.spec.js
@@ -1,31 +1,30 @@
-import { test, expect } from "@playwright/test"
-import { BaseLoginAPI } from "../pom/modules/api/loginApi"
-import { CustomersAPI } from "../pom/modules/api/customers"
+import { test, expect } from "@playwright/test";
+import { BaseLoginAPI } from "../pom/modules/api/loginApi";
+import { CustomersAPI } from "../pom/modules/api/customers";
 import { VALID_LOGIN_PAYLOAD } from "../fixtures/userData";
-import { generateRandomNumber } from "../fixtures/utils"
+import {generateRandomString } from "../fixtures/utils"
 
 test.describe("customers API tests", () => {
-    let loginAPI, customersAPI;
-  
-    test.beforeEach("get auth token", async ({ page }) => {
-      loginAPI = new BaseLoginAPI(page);
-      const loginResponse = await loginAPI.login(VALID_LOGIN_PAYLOAD);
-      customersAPI = new CustomersAPI(page, loginResponse.auth.token);
+  let loginAPI, customersAPI, userId;
+
+  test.beforeEach("get auth token", async ({ page }) => {
+    loginAPI = new BaseLoginAPI(page);
+    const loginResponse = await loginAPI.login(VALID_LOGIN_PAYLOAD);
+    userId = loginResponse.user.id;
+    customersAPI = new CustomersAPI(page, loginResponse.auth.token);
+  });
+
+  test("should be able to update a customers billing info", async () => {
+    //const cardholderName = generateRandomString();
+    const customerToUpdate = await customersAPI.getCustomerBillingInfo(userId);
+    const response = await customersAPI.updateCustomerBillingInfo(userId, {
+            cardholder: `New ${customerToUpdate.billing_info.cardholder}`,
+            card_type: "Visa",
+            card_number: "4111111131111111",
+            cvv: 911,
+            card_expiration_date: "12/24",
     });
-
-    test("should be able to update a customers billing info", async () => {
-        const allCustomersResponse = await customersAPI.getAllCustomers();
-        const numberOfCustomers = allCustomersResponse.customers.length;
-        const randomId = generateRandomNumber(numberOfCustomers);
-        const customerToUpdate = await customersAPI.getCustomerBillingInfo(randomId);
-        const customerInfo = customerToUpdate.customer;
-        const response = await customersAPI.updateCustomerBillingInfo(customerInfo.id, {
-          first_name: `Updated ${customerInfo.first_name}`,
-        });
-        expect(customerInfo.first_name).not.toBe(response.customer.first_name);
-        expect(response.customer.first_name).toBe(
-          `Updated ${customerInfo.first_name}`
-        );
-      });
-
-})
+    console.log(response)
+    expect(customerToUpdate.billing_info.cardholder).not.toBe(response.billing_info.cardholder);
+  });
+});
